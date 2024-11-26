babc78c0b6611c13a41edc0e767a26038886c68d
Harrison Gillespie <110575518+Warry13@users.noreply.github.com>
2024-11-26 12:40:12 -0600

    Working dynamic OpenAI calls for profiles up to 30000 characters

--- OpenAI_API.py
+++ OpenAI_API.py
@@ -608,14 +608,9 @@ example_user_profile = {
 
 user_profile = '{"User3: {"Games": ["Minecraft", "Rocket League", "Stardew Valley", "SCP: Secret Laboratory"]], "Game Genres": ["Sandbox", "Building/Construction", "Action", "Horror"]}'
 
-def summarize_gaming_activity(profile_data):
-  #profile_string = json.dumps(profile_data)
+def gpt_call_first(profile_data):
 
-  # Define the prompt
-  # question = profile_string + "Using the inputted json of multiple reddit users profiles above, for each user return a list of 5 games and 5 game genres the user might be interested in in json format following this example '{user1: {Games: (List of names of games), Game Genres: (List of names of genres)}, ...}'"
-  profile_array = [profile_data[:5], profile_data[5:10]]
-  question = str(profile_array[0]) + " Using the inputted json of multiple reddit users profiles above, for each user return a list of up to 5 games the user definitely plays and up to 5 game genres the user might be interested in in json format following this example '{user1: {Games: (List of names of games), Game Genres: (List of names of genres)}, ...}'"
-  second_question = str(profile_array[1]) + " Using the inputted json of multiple reddit users profiles above, for each user return a list of up to 5 games the user definitely plays and up to 5 game genres the user might be interested in in json format following this example '{user1: {Games: (List of names of games), Game Genres: (List of names of genres)}, ...}'"
+  question = str(profile_data) + " Using the inputted json of multiple reddit users profiles above, for each user return a list of up to 5 games the user definitely plays and up to 5 game genres the user might be interested in in json format following this example '{user1: {Games: (List of names of games), Game Genres: (List of names of genres)}, ...}'"
   
   # Make the API call
   client = OpenAI(
@@ -633,27 +628,76 @@ def summarize_gaming_activity(profile_data):
       model="gpt-4o",
   )
 
-  second_chat_completion = client.chat.completions.create(
-      messages=[
-          {
-              "role": "user",
-              "content": second_question,
-          }
-      ],
-      model="gpt-4o",
+  response = chat_completion.choices[0].message.content
+  #response = json.dumps(response)
+  response = response.replace("json","")
+  response = response.split("```")[1]
+  response = json.loads(response)
+  #response = json.loads(response[3:-3])
+  return response
+
+def length_checker(profile_data):
+  if len(str(profile_data)) > 30000:
+    if len(profile_data) == 1:
+      return {}
+    x = int(len(profile_data)/2)
+    profile_data1 = profile_data[:x]
+    profile_data2 = profile_data[x:]
+    res1 = length_checker(profile_data1)
+    res2 = length_checker(profile_data2)
+    response = {**res1,**res2}
+    return response
+  else:
+    return gpt_call_first(profile_data)
+  
+
+
+def summarize_gaming_activity(profile_data):
+  #profile_string = json.dumps(profile_data)
+
+  # Define the prompt
+  # question = profile_string + "Using the inputted json of multiple reddit users profiles above, for each user return a list of 5 games and 5 game genres the user might be interested in in json format following this example '{user1: {Games: (List of names of games), Game Genres: (List of names of genres)}, ...}'"
+  #profile_array = [profile_data[:5], profile_data[5:10]]
+  #second_question = str(profile_array[1]) + " Using the inputted json of multiple reddit users profiles above, for each user return a list of up to 5 games the user definitely plays and up to 5 game genres the user might be interested in in json format following this example '{user1: {Games: (List of names of games), Game Genres: (List of names of genres)}, ...}'"
+  
+  # Make the API call
+  client = OpenAI(
+      # This is the default and can be omitted
+      api_key= open_ai_key(),
   )
 
-  response = chat_completion.choices[0].message.content
-  response = json.dumps(response)
-  response = response.replace("json","")
-  response = json.loads(response)
-  response = json.loads(response[3:-3])
-  response2 = second_chat_completion.choices[0].message.content
-  response2 = json.dumps(response2)
-  response2 = response2.replace("json","")
-  response2 = json.loads(response2)
-  response2 = json.loads(response2[3:-3])
-  response.update(response2)
+  # chat_completion = client.chat.completions.create(
+  #     messages=[
+  #         {
+  #             "role": "user",
+  #             "content": question,
+  #         }
+  #     ],
+  #     model="gpt-4o",
+  # )
+
+  # second_chat_completion = client.chat.completions.create(
+  #     messages=[
+  #         {
+  #             "role": "user",
+  #             "content": second_question,
+  #         }
+  #     ],
+  #     model="gpt-4o",
+  # )
+
+  # response = chat_completion.choices[0].message.content
+  # response = json.dumps(response)
+  # response = response.replace("json","")
+  # response = json.loads(response)
+  # response = json.loads(response[3:-3])
+  # response2 = second_chat_completion.choices[0].message.content
+  # response2 = json.dumps(response2)
+  # response2 = response2.replace("json","")
+  # response2 = json.loads(response2)
+  # response2 = json.loads(response2[3:-3])
+  #response.update(response2)
+  response = length_checker(profile_data)
 
   question2 = str(response) + " Use this outputted json to select only the 5 users that best matches the profile below based on the similarity between the games they play and game genres they play returning only a json of the list of users in order of similarity with all the games and game genres they play in json format following this example '{user1: {Games: (List of names of games), Game Genres: (List of names of genres)}, ...}' " + str(user_profile)
 
